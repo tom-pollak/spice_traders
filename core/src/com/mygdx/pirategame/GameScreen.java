4f448585bb30995ed737862258300a1494708cb3
sp1665 <sp1665@york.ac.uk>
2022-01-28 12:03:00 +0000

    flip textures

--- core/src/com/mygdx/pirategame/GameScreen.java
+++ core/src/com/mygdx/pirategame/GameScreen.java
@@ -22,8 +22,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class GameScreen implements Screen {
-    private static float maxSpeed = 2;
-    private static float accel = 0.02f;
+    private static float maxSpeed = 3;
+    private static float accel = 0.2f;
 
     protected PirateGame game;
     private OrthographicCamera camera;
@@ -79,11 +79,11 @@ public class GameScreen implements Screen {
         // Spawning enemy ship and coin. x and y is spawn location
         colleges.put("Alcuin", new College(this, 1900 / PirateGame.PPM, 2100 / PirateGame.PPM,
                 "alcuin_flag.png", "alcuin_ship.png", 4));
-        colleges.put("Anne Lister", new College(this, 6250 / PirateGame.PPM, 1200 / PirateGame.PPM,
+        colleges.put("Anne Lister", new College(this, 6250 / PirateGame.PPM, 1250 / PirateGame.PPM,
                 "anne_lister_flag.png", "anne_lister_ship.png", 4));
-        colleges.put("Constantine", new College(this, 6500 / PirateGame.PPM, 6750 / PirateGame.PPM,
+        colleges.put("Constantine", new College(this, 5900 / PirateGame.PPM, 6750 / PirateGame.PPM,
                 "constantine_flag.png", "constantine_ship.png", 4));
-        colleges.put("Derwent", new College(this, 1900 / PirateGame.PPM, 6750 / PirateGame.PPM,
+        colleges.put("Derwent", new College(this, 1900 / PirateGame.PPM, 6500 / PirateGame.PPM,
                 "derwent_flag.png", "derwent_ship.png", 4));
         ships.addAll(colleges.get("Alcuin").fleet);
         ships.addAll(colleges.get("Anne Lister").fleet);
