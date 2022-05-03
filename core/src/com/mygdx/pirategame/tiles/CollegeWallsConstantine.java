d80a45c20ca98474a736d7c3e23db165e8b91dfb
Tom Pollak <tompollak1000@gmail.com>
2022-05-03 13:14:43 +0100

    Renamed collegeWalls to describe the college they are referencing

---
+++ core/src/com/mygdx/pirategame/tiles/CollegeWallsConstantine.java
@@ -0,0 +1,41 @@
+package com.mygdx.pirategame.tiles;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Rectangle;
+import com.mygdx.pirategame.PirateGame;
+import com.mygdx.pirategame.screens.GameScreen;
+import com.mygdx.pirategame.sprites.Entity;
+
+/**
+ * College Walls (Constantine) Checks interaction with walls from map
+ *
+ * @author Ethan Alabaster, Sam Pearson
+ * @version 1.0
+ */
+public class CollegeWallsConstantine extends InteractiveTileObject {
+    private final GameScreen screen;
+
+    /**
+     * Sets bounds of college walls
+     *
+     * @param screen Visual data
+     * @param bounds Wall bounds
+     */
+    public CollegeWallsConstantine(GameScreen screen, Rectangle bounds) {
+        super(screen, bounds);
+        this.screen = screen;
+        fixture.setUserData(this);
+        // Set the category bit
+        setCategoryFilter(PirateGame.COLLEGE_SENSOR_BIT);
+    }
+
+    /**
+     * Checks for contact with cannonball
+     */
+    @Override
+    public void onContact(Entity collidingEntity) {
+        Gdx.app.log("wall", "collision");
+        // Deal damage to the assigned college
+        screen.getCollege("Constantine").onContact(collidingEntity);
+    }
+}
